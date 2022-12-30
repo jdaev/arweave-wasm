023e804a4fe6a9ec5c8a65125338a43585a7bacf
Javed Hussein <jdhn42@gmail.com>
2022-12-29 20:39:50 -0500

    feat: add wallet gen

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,124 @@
+use arweave_wasm::Wallet;
+use clap::Parser;
+use colored::*;
+use hmac_drbg::HmacDRBG;
+use path_slash::PathBufExt as _;
+use rand::SeedableRng;
+use rand_chacha::ChaChaRng;
+use regex::Regex;
+use rsa::{RsaPrivateKey, RsaPublicKey};
+use sha2::{digest::generic_array::ArrayLength, Sha256};
+use std::fs;
+use std::path::PathBuf;
+use std::sync::mpsc;
+use std::thread;
+use std::time::Instant;
+use update_informer::{registry, Check};
+#[derive(Parser)]
+#[clap(author, version = {option_env!("RELEASE_VERSION").unwrap_or("dev")}, about, arg_required_else_help(true))]
+struct Args {
+    /// The number of threads to use
+    #[clap(short, long, value_parser, default_value_t = 8)]
+    threads: u8,
+
+    /// The path to the output directory
+    #[clap(short, long, value_parser, default_value("./wallets"))]
+    output: String,
+
+    /// Pattern to use for the wallet names
+    #[clap(value_parser)]
+    pattern: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    // Create a ChaChaRng random number generator using the seed
+    let mut seed =
+        "shrimp pony traffic photo favorite plastic fancy gadget february surge surface innocent"
+            .as_bytes();
+
+    
+    let current_version = option_env!("RELEASE_VERSION").unwrap_or("dev");
+
+    let informer = update_informer::new(registry::GitHub, "maximousblk/wave.rs", current_version);
+
+    if let Some(version) = informer.check_version().ok().flatten() {
+        let msg = format!(
+            "A new release of {} is available: v{} -> {}",
+            "wave".italic().cyan(),
+            current_version,
+            version.to_string().green()
+        );
+
+        let release_url = format!(
+            "https://github.com/maximousblk/wave.rs/releases/tag/{}",
+            version
+        )
+        .yellow();
+
+        println!("\n{msg}\n{url}", msg = msg, url = release_url);
+    }
+
+    let args = Args::parse();
+
+    let pattern = Regex::new(&args.pattern).expect("Invalid pattern");
+
+    let outdir = PathBuf::from(&args.output);
+
+    if !outdir.exists() || !outdir.is_dir() {
+        fs::create_dir_all(&outdir).expect("Failed to create output directory");
+    }
+
+    println!("\n🔍 Pattern: {}", format!("/{}/", &args.pattern).yellow());
+    println!(
+        "📁 Output directory: {}",
+        &outdir.to_slash_lossy().to_string().yellow()
+    );
+    println!("🧵 Threads: {}\n", &args.threads.to_string().yellow());
+
+    let (tx, rx) = mpsc::channel();
+
+    let now = Instant::now();
+
+    for t in 0..args.threads {
+        let tx = tx.clone();
+        thread::spawn(move || loop {
+            tx.send((t, Wallet::new())).expect("Failed to send jwt");
+        });
+    }
+
+    let mut count: u32 = 0;
+    for (t, wallet) in rx {
+        count += 1;
+        let address = &wallet.address();
+        if pattern.is_match(address) {
+            println!(
+                "\n✅ {} wallet: {}",
+                format!("[T{:02}]", t).dimmed(),
+                address.green().bold()
+            );
+
+            // write wallet jwk to file
+            let outfile = outdir.join(format!("arweave-keyfile-{}.json", address));
+
+            let jwk_json = serde_json::to_string(&wallet.jwk()).expect("Failed to serialize jwk");
+
+            fs::write(&outfile, jwk_json).expect("Failed to write wallet to file");
+
+            println!(
+                "📄 wallet written to file {}\n",
+                outfile.to_slash_lossy().to_string().yellow()
+            );
+
+            let el = now.elapsed().as_secs_f32();
+            println!(
+                "🏁 Done in {:.3}s {}",
+                el.to_string().bold(),
+                format!("({:.1}/s)", (count as f32 / el)).dimmed()
+            );
+
+            break;
+        } else {
+            println!("🔍 {} wallet: {}", format!("[T{:02}]", t).dimmed(), address);
+        }
+    }
 }
